2c234aee37bf75f491178e5e9f9daedc41ca0b9c
wangxiaochuan <wangxiaochuan01@163.com>
2022-07-02 10:57:09 +0800

    update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,42 +1,284 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh LpR lFf">
+    <q-header reveal elevated :class="$q.dark.isActive ? 'header_dark' : 'header_normal'">
       <q-toolbar>
         <q-btn
           flat
           dense
           round
           icon="menu"
-          aria-label="Menu"
+          class="q-mr-sm"
           @click="toggleLeftDrawer"
         />
+<!--        <q-avatar>-->
+<!--          <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">-->
+<!--        </q-avatar>-->
 
         <q-toolbar-title>
-          Quasar App
+          纳昂达可视化解读系统
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn
+          class="q-mr-xs"
+          flat
+          round
+          @click="$q.dark.toggle()"
+          :icon="$q.dark.isActive ? 'nights_stay' : 'wb_sunny'"
+        />
+        <a
+          style="font-size: 25px;"
+          class="float-right q-mr-sm"
+          href="https://github.com/sponsors/mayank091193"
+          target="_blank"
+          title="Donate"
+        ><i class="fas fa-heart" style="color: #eb5daa"></i
+        ></a>
+        <q-btn flat round dense icon="search" class="q-mr-xs" />
+        <q-btn
+          flat
+          round
+          dense
+          icon="fas fa-sign-out-alt"
+          @click="logoutNotify"
+          to="/"
+        />
       </q-toolbar>
     </q-header>
 
     <q-drawer
+      class="left-navigation text-white"
       v-model="leftDrawerOpen"
+      style="background-image: url(https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-2.32103624.jpg) !important;"
       show-if-above
-      bordered
+      side="left"
+      elevated
     >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+<!--      <q-list>-->
+<!--        <q-item-label-->
+<!--          header-->
+<!--        >-->
+<!--          Essential Links-->
+<!--        </q-item-label>-->
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+<!--        <EssentialLink-->
+<!--          v-for="link in essentialLinks"-->
+<!--          :key="link.title"-->
+<!--          v-bind="link"-->
+<!--        />-->
+<!--      </q-list>-->
+      <div
+        class="full-height"
+        :class="$q.dark.isActive ? 'drawer_dark' : 'drawer_normal'"
+      >
+        <div style="height: calc(100% - 117px);padding:10px;">
+          <q-toolbar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+
+            <q-toolbar-title>生信分析</q-toolbar-title>
+          </q-toolbar>
+          <hr />
+          <q-scroll-area style="height:100%;">
+            <q-list padding>
+              <q-item
+                active-class="tab-active"
+                to="/dashboard"
+                exact
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="dashboard" />
+                </q-item-section>
+
+                <q-item-section>
+                  Dashboard v1
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/dashboard_v2"
+                exact
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="dashboard" />
+                </q-item-section>
+
+                <q-item-section>
+                  Dashboard v2
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/dashboard_v3"
+                exact
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="dashboard" />
+                </q-item-section>
+
+                <q-item-section>
+                  Dashboard v3
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/customer_management"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="star" />
+                </q-item-section>
+
+                <q-item-section>
+                  Customer Management
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/change_request"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="send" />
+                </q-item-section>
+
+                <q-item-section>
+                  Change Request
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/sales_invoices"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="attach_money" />
+                </q-item-section>
+
+                <q-item-section>
+                  Sales Invoices
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/quotes"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="money" />
+                </q-item-section>
+
+                <q-item-section>
+                  Quotes
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/transactions"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="assignment" />
+                </q-item-section>
+
+                <q-item-section>
+                  Transactions
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/employee_salary_list"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="list" />
+                </q-item-section>
+
+                <q-item-section>
+                  Employee Salary List
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/calendar"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="calendar_today" />
+                </q-item-section>
+
+                <q-item-section>
+                  Calendar
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/department"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="business" />
+                </q-item-section>
+
+                <q-item-section>
+                  Department
+                </q-item-section>
+              </q-item>
+
+              <q-item
+                active-class="tab-active"
+                to="/my_profile"
+                class="q-ma-sm navigation-item"
+                clickable
+                v-ripple
+              >
+                <q-item-section avatar>
+                  <q-icon name="drafts" />
+                </q-item-section>
+
+                <q-item-section>
+                  My Profile
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-scroll-area>
+        </div>
+      </div>
     </q-drawer>
 
     <q-page-container>
@@ -47,7 +289,7 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+// import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
@@ -56,53 +298,53 @@ const linksList = [
     icon: 'school',
     link: 'https://quasar.dev'
   },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+  // {
+  //   title: 'Github',
+  //   caption: 'github.com/quasarframework',
+  //   icon: 'code',
+  //   link: 'https://github.com/quasarframework'
+  // },
+  // {
+  //   title: 'Discord Chat Channel',
+  //   caption: 'chat.quasar.dev',
+  //   icon: 'chat',
+  //   link: 'https://chat.quasar.dev'
+  // },
+  // {
+  //   title: 'Forum',
+  //   caption: 'forum.quasar.dev',
+  //   icon: 'record_voice_over',
+  //   link: 'https://forum.quasar.dev'
+  // },
+  // {
+  //   title: 'Twitter',
+  //   caption: '@quasarframework',
+  //   icon: 'rss_feed',
+  //   link: 'https://twitter.quasar.dev'
+  // },
+  // {
+  //   title: 'Facebook',
+  //   caption: '@QuasarFramework',
+  //   icon: 'public',
+  //   link: 'https://facebook.quasar.dev'
+  // },
+  // {
+  //   title: 'Quasar Awesome',
+  //   caption: 'Community Quasar projects',
+  //   icon: 'favorite',
+  //   link: 'https://awesome.quasar.dev'
+  // }
 ]
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
+    // EssentialLink
   },
 
   setup () {
-    const leftDrawerOpen = ref(false)
+    const leftDrawerOpen = ref(true)
 
     return {
       essentialLinks: linksList,
@@ -111,6 +353,54 @@ export default defineComponent({
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
     }
+  },
+  methods: {
+    logoutNotify() {
+      this.$q.notify({
+        message: "Logged out"
+      });
+    }
   }
 })
 </script>
+
+
+<style>
+.q-drawer {
+  /*background-image: url(https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-2.32103624.jpg) !important;*/
+  background-image: url("/statics/images/lake.jpg") !important;
+  background-size: cover !important;
+}
+
+.drawer_normal {
+  background-color: rgba(1, 1, 1, 0.75);
+}
+
+.drawer_dark {
+  background-color: #010101f2;
+}
+
+.navigation-item {
+  border-radius: 5px;
+}
+
+.tab-active {
+  background-color: green;
+}
+
+body {
+  background: #f1f1f1 !important;
+}
+
+.header_normal {
+  background: linear-gradient(
+    145deg,
+    rgb(32, 106, 80) 15%,
+    rgb(21, 57, 102) 70%
+  );
+}
+
+.header_dark {
+  background: linear-gradient(145deg, rgb(61, 14, 42) 15%, rgb(14, 43, 78) 70%);
+}
+</style>
