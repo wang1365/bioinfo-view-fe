7ca28fcf6bf5eb3d081672220471255bf057802c
Mering.Gao <canhuayin@gmail.com>
2022-08-22 22:39:27 +0800

    增加 docker 配置

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+GIT_BRANCH="$(git branch --show-current)"
+GIT_HEAD="$(git rev-parse --short=7 HEAD)"
+GIT_DATE=$(git log HEAD -n1 --pretty='format:%cd' --date=format:'%Y%m%d-%H%M')
+
+# REPO="镜像仓库"
+# CONTAINER="镜像名"
+# TAG="${GIT_BRANCH}-${GIT_HEAD}-${GIT_DATE}"
+# DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
+
+DOCKER_IMAGE="frontend"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILDROOT=$DIR
+# Build staic
+# ng build --prod --deploy-url=/url前缀/
+# Build docker
+
+quasar build
+
+cmd="docker build \
+  -t $DOCKER_IMAGE \
+  -f $DIR/Dockerfile $BUILDROOT"
+echo $cmd
+eval $cmd
+
