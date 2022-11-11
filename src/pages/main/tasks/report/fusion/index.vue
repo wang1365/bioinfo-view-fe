4138e127e114cb03ebed13d3d79890d2c1d6f488
wangxiaochuan <wangxiaochuan01@163.com>
2022-11-11 10:45:24 +0800

    update

--- src/pages/main/tasks/report/fusion/index.vue
+++ src/pages/main/tasks/report/fusion/index.vue
@@ -1,13 +1,25 @@
 <template>
-    <div class="q-py-md">
-        融合分析介绍:融合分析介绍:融合分析介绍:融合分析介绍:融合分析介绍:融合分析介绍:融合分析介绍:融合分析介绍:
-    </div>
-    <div class="q-py-md">
-        <q-tabs v-model="tab" active-color="primary" active-bg-color="grey-4" align="left" class="bg-grey-1"
-            :breakpoint="0" dense>
+    <div class="q-py-sm">
+        <q-btn
+            icon="help_outline"
+            size="small"
+            outline
+            color="orange"
+            class="relative-position float-right z-top q-mr-md"
+            @click="dlgVisible = !dlgVisible"
+            >说明</q-btn
+        >
+        <q-tabs
+            v-model="tab"
+            active-color="primary"
+            active-bg-color="grey-4"
+            align="left"
+            class="bg-grey-1"
+            :breakpoint="0"
+            dense
+        >
             <q-tab name="单样品融合分析" label="单样品融合分析" />
             <q-tab name="体细胞融合分析" label="体细胞融合分析" />
-
         </q-tabs>
         <q-tab-panels v-model="tab" animated>
             <q-tab-panel name="单样品融合分析">
@@ -17,11 +29,27 @@
                 <NormalVue />
             </q-tab-panel>
         </q-tab-panels>
+        <q-separator class="bg-separator" />
+        <div class="q-py-md">
+            {{ intro }}
+        </div>
+        <q-dialog v-model="dlgVisible">
+            <q-card style="width: 800px; max-width: 2000px">
+                <q-bar class="bg-primary text-white">融合分析解读</q-bar>
+                <q-card-section>{{intro}}</q-card-section>
+                <q-card-actions align="center">
+                    <q-btn v-close-popup color="primary">关闭</q-btn>
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
     </div>
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
 import NormalVue from "./Normal.vue";
 import TumorVue from "./Tumor.vue";
+
 const tab = ref("单样品融合分析")
+const dlgVisible = ref(false)
+const intro = ref('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 </script>
--- src/pages/main/tasks/report/qc/index.vue
+++ src/pages/main/tasks/report/qc/index.vue
@@ -1,17 +1,25 @@
 <template>
-
-    <div class="q-py-md">
-        {{qcIntro}}
-    </div>
-
-    <div class="q-py-md">
-
-
-        <q-tabs v-model="tab" active-color="primary" active-bg-color="grey-4" align="left" class="bg-grey-1"
-            :breakpoint="0" dense>
+    <div class="q-py-sm">
+        <q-btn
+            icon="help_outline"
+            size="small"
+            outline
+            color="orange"
+            class="relative-position float-right z-top q-mr-md"
+            @click="dlgVisible = !dlgVisible"
+            >说明</q-btn
+        >
+        <q-tabs
+            v-model="tab"
+            active-color="primary"
+            active-bg-color="grey-4"
+            align="left"
+            class="bg-grey-1"
+            :breakpoint="0"
+            dense
+        >
             <q-tab name="试剂盒捕获质控信息" label="试剂盒捕获质控信息" />
             <q-tab name="深度信息" label="深度信息" />
-
         </q-tabs>
         <q-tab-panels v-model="tab" animated>
             <q-tab-panel name="试剂盒捕获质控信息">
@@ -21,26 +29,38 @@
                 <DeepInfoVue />
             </q-tab-panel>
         </q-tab-panels>
+        <q-separator class="bg-separator" />
+        <div class="q-py-md">
+            {{ intro }}
+        </div>
+        <q-dialog v-model="dlgVisible">
+            <q-card style="width: 800px; max-width: 2000px">
+                <q-bar class="bg-primary text-white">质控解读</q-bar>
+                <q-card-section>{{intro}}</q-card-section>
+                <q-card-actions align="center">
+                    <q-btn v-close-popup color="primary">关闭</q-btn>
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
     </div>
-
 </template>
 <script setup>
-import { ref, onMounted } from "vue";
+import {ref, onMounted} from "vue";
 import KitCaptureVue from "./KitCapture.vue"
 import DeepInfoVue from "./DeepInfo.vue"
-import { useRoute } from 'vue-router'
-import { getReportText } from "src/api/report"
+import {useRoute} from 'vue-router'
+import {getReportText} from "src/api/report"
 
 const route = useRoute()
 const tab = ref("试剂盒捕获质控信息")
-const qcIntro = ref('')
+const intro = ref('')
+const dlgVisible = ref(false)
 
 
 onMounted(() => {
     getReportText(route.params.id, 'QC_TIP').then(res => {
         console.log('====>', res)
-        qcIntro.value = res
+        intro.value = res
     })
 })
-
 </script>
