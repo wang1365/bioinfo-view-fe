da67de0ac7d96fcf0c4b49bc6b697d12461e0266
Mering <canhuayin@gmail.com>
2023-04-06 09:27:42 +0800

    项目首页翻译

--- src/pages/main/projects/index.vue
+++ src/pages/main/projects/index.vue
@@ -1,6 +1,6 @@
 <template>
     <q-page padding style="overflow-x: hidden">
-        <PageTitle title="项目管理" />
+        <PageTitle :title="$t('ProjectPageIndexTitle')" />
         <ProjectsList />
     </q-page>
 </template>
