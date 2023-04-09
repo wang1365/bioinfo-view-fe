9ae7c32cdfbe153bb4cf9bd3022395865ac05d4f
wangxiaochuan <wangxiaochuan01@163.com>
2023-04-09 15:07:13 +0800

    ant design vue支持国际化切换

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
-    <a-config-provider :locale="locale">
+    <a-config-provider :locale="antLocale" >
         <router-view />
     </a-config-provider>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import zhCN from 'ant-design-vue/es/locale/zh_CN';
-import moment from "moment";
-import "moment/locale/zh-cn";
+import { defineComponent } from "vue"
+import cn from 'ant-design-vue/es/locale/zh_CN'
+import en from 'ant-design-vue/es/locale/en_US'
+import moment from "moment"
+import "moment/locale/zh-cn"
+import { globalStore } from "stores/global"
 
 moment.locale("zh-cn");
 export default defineComponent({
     name: "App",
     data() {
         return {
-            locale: zhCN,
-        };
+        }
+    },
+    computed: {
+      antLocale: () => {
+          const store = globalStore()
+          return store.lang === 'zh-CN' ? cn : en
+      }
     },
 });
 </script>
