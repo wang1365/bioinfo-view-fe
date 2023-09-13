475eb7224ab8cf525353c5692c87de7739f08a0f
Mering <Mering>
2023-09-13 22:12:57 +0800

    feat: 增加目录结构

---
+++ src/pages/main/tasks/report/pathogeneis-backterial/index.vue
@@ -0,0 +1,68 @@
+<template>
+     <div>
+            <q-btn
+                v-if="props.viewConfig.showStick && props.viewConfig.stickDone"
+                icon="bookmarks"
+                size="small"
+                color="primary"
+                class="relative-position float-right q-mr-md"
+                :label="$t('ReportStickDone')"
+                @click="reset()"
+            />
+            <q-btn
+                v-if="props.viewConfig.showStick && !props.viewConfig.stickDone"
+                icon="bookmarks"
+                size="small"
+                outline
+                color="primary"
+                class="relative-position float-right q-mr-md"
+                @click="stickFilter()"
+                :label="$t('ReportStickData')"
+            />
+            <q-btn
+                icon="help_outline"
+                size="small"
+                outline
+                color="orange"
+                class="relative-position float-right q-mr-md"
+                @click="dlgVisible = !dlgVisible"
+                >{{ $t('Intro') }}</q-btn
+            >
+     </div>
+
+</template>
+
+<script setup>
+import { computed, toRef, onMounted,ref,watch } from "vue";
+const props = defineProps({
+    intro: {
+        type: String,
+        required: false,
+    },
+    samples: {
+        type: Array,
+        required: false,
+        default: () => [],
+    },
+    viewConfig: {
+        type: Object,
+        required: false,
+        default() {
+            return {
+                showMutGermline: true,
+                showMutSomatic: true,
+                showSticky: false,
+            }
+        },
+    },
+    task: {
+        type: Object,
+        required: false,
+    },
+    stepData: {
+        type: Object,
+        default: () => {}
+    }
+})
+
+</script>
