06d90548ffff06ef44e304ebe77c9ae289db37c0
Wang, Xiaochuan <wangxiaochuan01@163.com>
2024-12-30 15:53:50 +0800

    任务详情，样本列表使用表格展示

--- src/pages/main/tasks/TaskDetail/index.vue
+++ src/pages/main/tasks/TaskDetail/index.vue
@@ -2,14 +2,14 @@
     <q-page padding>
         <div class="row items-center q-py-sm">
             <h6>{{ `${$t('Task')}${$t('Info')}` }}</h6>
-            <q-space />
-            <q-btn :label="$t('Back')" icon="arrow_back" color="primary" @click="router.back()" />
+            <q-space/>
+            <q-btn :label="$t('Back')" icon="arrow_back" color="primary" @click="router.back()"/>
         </div>
         <q-card>
             <q-card-section>
                 <div class="row">
                     <div class="text-h6 q-pa-xs col-3">
-                        <span class="text-primary text-weight-bolder">{{ $t('Project')  + ": "}}</span>
+                        <span class="text-primary text-weight-bolder">{{ $t('Project') + ": " }}</span>
                         <span class="text-h6 bg-grey-3 q-pa-sm">{{ taskDetail.project?.name }}</span>
                     </div>
                     <div class="text-h6 q-pa-xs col-3">
@@ -17,36 +17,39 @@
                         <span class="text-h6 bg-grey-3 q-pa-sm">{{ taskDetail.name }}</span>
                     </div>
                     <div class="text-h6 q-pa-xs col-3">
-                        <span class="text-primary text-weight-bolder">{{ `${$t('Task')}${$t('Status')}` + ": "}}</span>
+                        <span class="text-primary text-weight-bolder">{{ `${$t('Task')}${$t('Status')}` + ": " }}</span>
                         <span
                             class="q-px-md brand-color text-center row inline flex-center text-white rounded-borders"
                             :class="getItemStatusColor(taskDetail)"
-                            >{{ getItemStatus(taskDetail) }}</span
+                        >{{ getItemStatus(taskDetail) }}</span
                         >
                     </div>
                 </div>
             </q-card-section>
             <q-separator></q-separator>
             <q-card-section>
-                <div class="text-h6 q-pa-xs"><span class="text-primary text-weight-bolder">{{ $t('Sample') + ": "}}</span></div>
-                <div class="text-body q-pa-xs">
-                    <a-table
-                        :columns="columns"
-                        :data-source="taskSamples"
-                    >
-                    </a-table>
+                <div class="text-h6 q-pa-xs"><span
+                    class="text-primary text-weight-bolder">{{ $t('Sample') + ": " }}</span>
                 </div>
+                <a-table
+                    :columns="columns"
+                    :data-source="taskSamples"
+                />
             </q-card-section>
             <q-separator></q-separator>
             <q-card-section>
-                <div class="text-h6 q-pa-xs"><span class="text-primary text-weight-bolder">{{ $t('ShellEnvs') + ": " }}</span></div>
+                <div class="text-h6 q-pa-xs"><span class="text-primary text-weight-bolder">{{
+                        $t('ShellEnvs') + ": "
+                    }}</span></div>
                 <div class="text-body q-px-md q-py-xs" v-for="item of taskEnvs" :key="item.key">
                     <span class="text-bold">{{ item.key }} : </span> {{ item.value }}
                 </div>
             </q-card-section>
             <q-separator></q-separator>
             <q-card-section>
-                <div class="text-h6 q-pa-xs"><span class="text-primary text-weight-bolder">{{ $t('Progress') + ": " }}</span></div>
+                <div class="text-h6 q-pa-xs"><span class="text-primary text-weight-bolder">{{
+                        $t('Progress') + ": "
+                    }}</span></div>
                 <div id="task-step" class="text-body q-px-md q-py-md">
                     <q-stepper v-model="lastStageIndex" color="primary">
                         <q-step
@@ -65,7 +68,8 @@
             </q-card-section>
             <q-separator></q-separator>
             <q-card-section>
-                <div class="text-h6 q-pa-xs"><span class="text-primary text-weight-bolder">{{ $t('Log') + ": " }}</span></div>
+                <div class="text-h6 q-pa-xs"><span class="text-primary text-weight-bolder">{{ $t('Log') + ": " }}</span>
+                </div>
                 <div class="text-body q-px-md q-py-xs">
                     <q-timeline color="secondary">
                         <q-timeline-entry
@@ -84,19 +88,20 @@
 
 <script setup>
 import PageTitle from "components/page-title/PageTitle.vue";
-import { useApi } from "src/api/apiBase";
+import {useApi} from "src/api/apiBase";
 import {computed, onMounted, ref, watch} from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { globalStore } from 'src/stores/global'
-import { useI18n } from "vue-i18n";
-import { storeToRefs } from 'pinia'
+import {useRoute, useRouter} from "vue-router";
+import {globalStore} from 'src/stores/global'
+import {useI18n} from "vue-i18n";
+import {storeToRefs} from 'pinia'
 import {toLocalString} from "src/utils/time"
-const store = globalStore()
-const { langCode } = storeToRefs(store)
 
-const { t } = useI18n();
+const store = globalStore()
+const {langCode} = storeToRefs(store)
+
+const {t} = useI18n();
 const router = useRouter()
-const { apiGet, apiGetByIds } = useApi();
+const {apiGet, apiGetByIds} = useApi();
 const route = useRoute();
 const taskDetail = ref({});
 const taskSamples = ref([])
@@ -108,25 +113,42 @@ const taskEnvs = ref([])
 
 const columns = computed(() => {
     return [
-        {title: `No.`, dataIndex:'index', align: 'center', customRender: ({index}) => index + 1}, //item.sample_meta?.patient?.name
-        {title: `${t('Patient')}${t('Name')}`, dataIndex: ['sample_meta','patient','name'], align: 'center'}, //item.sample_meta?.patient?.name
+        {title: `No.`, dataIndex: 'index', align: 'center', customRender: ({index}) => index + 1}, //item.sample_meta?.patient?.name
+        {
+            title: `${t('Patient')}${t('Name')}`, dataIndex: ['sample_meta', 'patient', 'name'], align: 'center',
+            sorter: (a, b) => a.sample_meta.patient.name.localeCompare(b.sample_meta.patient.name),
+        },
         {
             title: `${t('Patient')}${t('Gender')}`,
             dataIndex: 'k1',
             align: 'center',
-                customRender: ({text, record, index, column}) => {
-                    return record.sample_meta?.patient?.gender == '男' ? t('Male') : t('Female')
-                }
+            customRender: ({text, record, index, column}) => {
+                return record.sample_meta?.patient?.gender == '男' ? t('Male') : t('Female')
+            }
         },
-        {title: t('PatientNewFormPatientIdentificationNumber'), dataIndex: ['sample_meta','patient','identifier'], align: 'center'},
-        {title: t('SampleListTableColumnSampleIdentificationNumber'), dataIndex: ['sample_meta','identifier'], align: 'center'},
-        {title: t('SampleListTableColumnSamplingSite'), dataIndex: ['sample_meta','sample_componet'], align: 'center'},
-        {title: t('SampleListTableColumnTumorSample'), dataIndex: ['sample_meta','is_panel'], align: 'center',
+        {
+            title: t('PatientNewFormPatientIdentificationNumber'),
+            dataIndex: ['sample_meta', 'patient', 'identifier'],
+            align: 'center',
+            sorter: (a, b) => a.sample_meta.patient.identifier.localeCompare(b.sample_meta.patient.identifier),
+        },
+        {
+            title: t('SampleListTableColumnSampleIdentificationNumber'),
+            dataIndex: ['sample_meta', 'identifier'],
+            align: 'center',
+            sorter: (a, b) => a.sample_meta.identifier.localeCompare(b.sample_meta.identifier),
+        },
+        {title: t('SampleListTableColumnSamplingSite'), dataIndex: ['sample_meta', 'sample_componet'], align: 'center'},
+        {
+            title: t('SampleListTableColumnTumorSample'), dataIndex: ['sample_meta', 'is_panel'], align: 'center',
             customRender: ({text, record, index, column}) => {
                 return record.sample_meta?.is_panel ? 'true' : 'false'
             }
         },
-        {title: t('DataListTableColumnDataIdentificationNumber'), dataIndex: 'identifier', align: 'center'},
+        {
+            title: t('DataListTableColumnDataIdentificationNumber'), dataIndex: 'identifier', align: 'center',
+            sorter: (a, b) => a.identifier.localeCompare(b.identifier),
+        },
         {title: t('DataListTableColumnDataNameOfR1'), dataIndex: 'fastq1_path', align: 'center'},
         {title: t('DataListTableColumnDataNameOfR2'), dataIndex: 'fastq2_path', align: 'center'},
     ]
@@ -188,7 +210,7 @@ watch(
     }
 );
 watch(langCode, lc => {
-    logs.value=[]
+    logs.value = []
     buildTaskStageAndLog(taskDetail.value)
 })
 onMounted(() => {
@@ -239,12 +261,12 @@ const buildTaskEnvs = (taskDetail) => {
     if (store.currentUser.role_list.indexOf('super') < 0) {
         for (const key in taskDetail.env) {
             if (exclude.indexOf(key) < 0) {
-                taskEnvs.value.push({ "key": key, "value": taskDetail.env[key] })
+                taskEnvs.value.push({"key": key, "value": taskDetail.env[key]})
             }
         }
     } else {
         for (const key in taskDetail.env) {
-            taskEnvs.value.push({ "key": key, "value": taskDetail.env[key] })
+            taskEnvs.value.push({"key": key, "value": taskDetail.env[key]})
         }
     }
     console.log(taskEnvs.value)
