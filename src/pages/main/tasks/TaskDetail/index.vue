1a6e98ea2a8e7070329e78b3d035a545f9e78cbf
Wang, Xiaochuan <wangxiaochuan01@163.com>
2025-01-20 12:09:54 +0800

    任务详情。样本列表支持分页

--- src/pages/main/tasks/TaskDetail/index.vue
+++ src/pages/main/tasks/TaskDetail/index.vue
@@ -31,7 +31,7 @@
                 <div class="text-h6 q-pa-xs">
                     <span class="text-primary text-weight-bolder">{{ $t('Sample') + ": " }}</span>
                 </div>
-                <a-table :columns="columns" :data-source="taskSamples" size="small" :pagination="false" />
+                <a-table :columns="columns" :data-source="taskSamples" size="small" />
             </q-card-section>
             <q-separator></q-separator>
             <q-card-section>
