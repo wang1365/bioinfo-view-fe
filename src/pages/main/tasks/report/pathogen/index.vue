ef32f27810b1c11b043b50aadbe52951d70cf703
Wang, Xiaochuan <wangxiaochuan01@163.com>
2025-02-10 09:14:10 +0800

    rename to showColumnUniqReads

--- src/pages/main/tasks/report/pathogen/index.vue
+++ src/pages/main/tasks/report/pathogen/index.vue
@@ -203,7 +203,7 @@ const getSpan = (index, record) => {
 
 const showTotalProportion = computed(() => {
     console.log('============>>>>>', viewConfig.value)
-    const v = viewConfig.value?.showUniqueReads
+    const v = viewConfig.value?.showColumnUniqReads
     return v === undefined ? true : v
 })
 
