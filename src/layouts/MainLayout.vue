6f290eee2a3fe1a9b2e2482baf37e061462038ef
Wang, Xiaochuan <wangxiaochuan01@163.com>
2025-04-28 21:24:46 +0800

    修复多个y轴滚动条问题

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-layout view="hHh lpR lFf">
+    <q-layout view="hHh Lpr lff">
         <!--        <q-header reveal elevated class="bg-primary text-white header_normal">-->
         <q-header reveal elevated class="header_normal">
             <q-toolbar>
@@ -95,31 +95,14 @@
                         <template v-for="(item, index) in menuList" :key="index">
                             <SideBarLeftItem :addRoutesItem="item" :initLevel="0" />
                         </template>
-                        <!--                        <template-->
-                        <!--                            v-for="(menuItem, index) in menuList"-->
-                        <!--                            :key="index"-->
-                        <!--                        >-->
-                        <!--                            <q-item clickable v-ripple :to="menuItem.path">-->
-                        <!--                                <q-item-section avatar>-->
-                        <!--                                    <q-icon :name="menuItem.icon" />-->
-                        <!--                                </q-item-section>-->
-                        <!--                                <q-item-section>-->
-                        <!--                                    {{ menuItem.label }}-->
-                        <!--                                </q-item-section>-->
-                        <!--                            </q-item>-->
-                        <!--                            <q-separator-->
-                        <!--                                :key="'sep' + index"-->
-                        <!--                                v-if="menuItem.separator"-->
-                        <!--                            />-->
-                        <!--                        </template>-->
                     </q-list>
                 </q-scroll-area>
-                <!-- drawer content -->
             </div>
         </q-drawer>
 
         <q-page-container>
-            <div style="height:calc(100vh - 50px);overflow-y: scroll;">
+            <div style="height:calc(100vh - 70px);overflow-y: scroll;">
+                <!--            <div style="overflow-y: scroll;">-->
                 <router-view />
             </div>
         </q-page-container>
@@ -200,9 +183,6 @@ onBeforeMount(() => {
     background-color: green;
 }
 
-body {
-    background: #f1f1f1 !important;
-}
 
 .header_normal {
     background: linear-gradient(
