93a68950870d8f19c4a4a08329ab370d2cc79489
wangxiaochuan <xiaochuan.wang@proaimltd.com>
2025-11-13 15:50:14 +0800

    自定义表格已选列表格内容优化

--- src/pages/main/tasks/defineReport/components/SelectedWesCnvTable.vue
+++ src/pages/main/tasks/defineReport/components/SelectedWesCnvTable.vue
@@ -8,15 +8,18 @@
       :loading="loading"
       :data-source="displayRows"
       :columns="columns"
-      :scroll="{ x: 600, y: 450 }"
+      :scroll="{ x: 2000, y: 450 }"
       rowKey="lineNumber"
       :sticky="true"
       :row-selection="rowSelection"
     >
       <template #bodyCell="{ column, record }">
-        <template v-if="column.dataIndex === 'rowNo'">
-          <div>{{ record.lineNumber }}</div>
+        <template v-if="column.dataIndex === 'expand'">
+          <div class="cursor-pointer" @click="record.expanded = !record.expanded">
+            <q-icon :name="record.expanded ? 'unfold_less' : 'unfold_more'" :color="record.expanded ? 'purple' : 'primary'" size="sm" />
+          </div>
         </template>
+        
         <template v-if="column.dataIndex === 'geneInfo'">
           <div>
             <div class="text-weight-bolder text-green-5" v-for="item of record.gene_data.slice(0, record.expanded ? record.gene_data.length : 2)" :key="item">
@@ -30,6 +33,37 @@
         <template v-if="column.dataIndex === 'copyNumber'">
           <div class="text-blue text-weight-bolder" v-if="record.CNV_Type === 'DUP'">{{ record.Copy_Number }} | {{ record.CNV_Type }}</div>
           <div class="text-red text-weight-bolder" v-if="record.CNV_Type === 'DEL'">{{ record.Copy_Number }} | {{ record.CNV_Type }}</div>
+        </template>
+        <template v-if="column.dataIndex === 'geneRelatedDiseases'">
+          <q-scroll-area style="height: 300px;" v-if="record.expanded">
+            <div v-for="item of record.gene_related_diseases_data" :key="item">{{ item }}</div>
+          </q-scroll-area>
+          <div v-else>
+            <div v-for="item of record.gene_related_diseases_data.slice(0, 2)" :key="item">{{ item }}</div>
+          </div>
+        </template>
+        <template v-if="column.dataIndex === 'userVerdict'">
+          <div>{{ (record.userVerdict || []).join(', ') || '-' }}</div>
+        </template>
+        <template v-if="column.dataIndex === 'acmg'">
+          <div>{{ Array.isArray(record.acmg_data) ? record.acmg_data.join(', ') : record.ACMG_result }}</div>
+        </template>
+        <template v-if="column.dataIndex === 'relatedHPOs'">
+          <q-scroll-area style="height: 300px;" v-if="record.expanded">
+            <div v-for="item of record.hpo_data" :key="item">{{ item }}</div>
+          </q-scroll-area>
+          <div v-else>
+            <div v-for="item of record.hpo_data.slice(0, 2)" :key="item">{{ item }}</div>
+          </div>
+        </template>
+        <template v-if="column.dataIndex === 'dgv'">
+          <div>{{ Array.isArray(record.dgv_data) ? record.dgv_data.join(', ') : record.DGV }}</div>
+        </template>
+        <template v-if="column.dataIndex === 'clinvar'">
+          <div>{{ Array.isArray(record.clinvar_data) ? record.clinvar_data.join(', ') : record.Clinvar }}</div>
+        </template>
+        <template v-if="column.dataIndex === 'plot'">
+          <div>-</div>
         </template>
         <template v-if="column.dataIndex === 'operation'">
           <q-btn color="negative" size="sm" :label="$t('Delete')" @click="$emit('remove', record.lineNumber)" />
@@ -45,6 +79,8 @@ import { ref, watch, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { readTaskFile } from 'src/api/task'
 import { parseCsvToList } from 'src/utils/csv'
+import { listVerdictByPatient } from 'src/api/verdict'
+import { globalStore } from 'src/stores/global'
 
 const props = defineProps({
   selectedKeys: { type: Array, default: () => [] },
@@ -59,14 +95,25 @@ const displayRows = ref([])
 const selectedRowKeys = ref([])
 
 const columns = [
-  { title: 'Row', dataIndex: 'rowNo', width: 80, align: 'center' },
+  { title: '', dataIndex: 'expand', width: 50, align: 'left' },
   { title: 'Gene Info', dataIndex: 'geneInfo', width: 260, align: 'left' },
   { title: 'Copy Number', dataIndex: 'copyNumber', width: 200, align: 'left' },
-  { title: 'Operation', dataIndex: 'operation', width: 120, align: 'center' },
+  { title: 'Gene Related Diseases', dataIndex: 'geneRelatedDiseases', width: 240, align: 'left' },
+  { title: 'User Verdict', dataIndex: 'userVerdict', width: 200, align: 'left' },
+  { title: 'ACMG', dataIndex: 'acmg', width: 150, align: 'left' },
+  { title: 'Related HPOs', dataIndex: 'relatedHPOs', width: 300, align: 'left' },
+  { title: 'DGV', dataIndex: 'dgv', width: 200, align: 'left' },
+  { title: 'Clinvar', dataIndex: 'clinvar', width: 300, align: 'left' },
+  { title: 'Plot', dataIndex: 'plot', width: 200, align: 'left' },
+  { title: 'Operation', dataIndex: 'operation', width: 120, align: 'center', fixed: 'right' },
 ]
 
 const loadData = async () => {
   loading.value = true
+  const store = globalStore()
+  const verdicts = await listVerdictByPatient(store.currentUser.username)
+  const verdictMap = new Map()
+  verdicts.forEach(v => verdictMap.set(v.gene_identifier, v.result))
   const patient = props.samples && props.samples.length > 0 ? props.samples[0].identifier : ''
   const res = await readTaskFile(route.params.id, `CNV_WES/${patient}.CNV_WES.txt`)
   const data = parseCsvToList(res)
@@ -75,6 +122,16 @@ const loadData = async () => {
     rec.lineNumber = idx + 1
     rec.expanded = false
     rec.gene_data = (rec.Gene || '').split(';').filter((t) => t)
+    rec.gene_related_diseases_data = (rec.Gene_Related_Diseases || '').split(';').filter((t) => t && t !== '.')
+    const gid = `${rec['Gene']}|${rec['Chr']}|${rec['Start']}|${rec['End']}|${rec['CNV_Type']}`.slice(-99)
+    rec.geneIdentifier = gid
+    rec.userVerdict = verdictMap.get(gid) || []
+    rec.hpo_data = (rec.HPO || '').split(';').filter((t) => t)
+    rec.dgv_data = (rec.DGV || '').split(';').map(x => x && x.replace(',', '')).filter((t) => t)
+    rec.clinvar_data = (rec.Clinvar || '').split(';').map(x => x && x.replace(',', '')).filter((t) => t)
+    rec.acmg_data = []
+    if (rec.ACMG_result) rec.acmg_data.push(rec.ACMG_result)
+    if (rec.ACMG) rec.ACMG.split(';').forEach(e => rec.acmg_data.push(e))
     return rec
   })
   allRows.value = rows
--- src/pages/main/tasks/defineReport/components/SelectedWesMutationTable.vue
+++ src/pages/main/tasks/defineReport/components/SelectedWesMutationTable.vue
@@ -1,109 +1,220 @@
 <template>
-  <div>
-    <div class="row items-center q-pb-sm q-gutter-sm">
-      <q-btn color="primary" :label="$t('Add')" icon="add" @click="$emit('openAdd')" />
-      <q-btn color="negative" :label="$t('Delete')" icon="delete" @click="bulkDelete" :disable="selectedRowKeys.length === 0" />
+    <div>
+        <div class="row items-center q-pb-sm q-gutter-sm">
+            <q-btn color="primary" :label="$t('Add')" icon="add" @click="$emit('openAdd')" />
+            <q-btn
+                color="negative"
+                :label="$t('Delete')"
+                icon="delete"
+                @click="bulkDelete"
+                :disable="selectedRowKeys.length === 0"
+            />
+        </div>
+        <a-table
+            :loading="loading"
+            :data-source="displayRows"
+            :columns="columns"
+            :scroll="{ x: 2000, y: 450 }"
+            rowKey="lineNumber"
+            :sticky="true"
+            :row-selection="rowSelection"
+        >
+            <template #bodyCell="{ column, record }">
+                <template v-if="column.dataIndex === 'expand'">
+                    <div class="cursor-pointer" @click="record.expanded = !record.expanded">
+                        <q-icon
+                            :name="record.expanded ? 'unfold_less' : 'unfold_more'"
+                            :color="record.expanded ? 'purple' : 'primary'"
+                            size="sm"
+                        />
+                    </div>
+                </template>
+
+                <template v-if="column.dataIndex === 'geneInfo'">
+                    <div class="row">
+                        <template v-if="record.expanded">
+                            <div class="col-4 column justify-between">
+                                <div
+                                    class="col-2 text-weight-bolder text-green-5 q-pl-sm justify-between"
+                                    style="font-size: 14px"
+                                >
+                                    {{ record.Class }}
+                                </div>
+                            </div>
+                            <div class="col-8 text-left">
+                                <div class="text-primary" style="font-size: 16px">{{ record['Gene.refGene'] }}</div>
+                                <div>{{ record['ExonicFunc.refGene'] }}</div>
+                                <div>
+                                    {{ record['Chr'] + ':' + record.Start + ' ' + record.Ref + '>' + record.Alt }}
+                                </div>
+                                <div>{{ record.NUChange }}</div>
+                                <div>{{ record.AAChange }}</div>
+                                <div>{{ record['GeneDetail.refGene'] + ' ' + record.exon }}</div>
+                            </div>
+                        </template>
+                        <template v-else>
+                            <div class="col-4">
+                                <div class="text-weight-bolder text-green-5 q-pl-sm" style="font-size: 16px">
+                                    {{ record.Class }}
+                                </div>
+                                <div class="text-primary content-center" style="font-size: 16px">
+                                    {{ record['Gene.refGene'] }}
+                                </div>
+                            </div>
+                            <div class="col-8 text-blue-grey-6 justify-between">
+                                <div>{{ record['ExonicFunc.refGene'] }}</div>
+                                <div>{{ record.NUChange }}</div>
+                            </div>
+                        </template>
+                    </div>
+                </template>
+                <template v-if="column.dataIndex === 'genoTypeQuality'">
+                    <template v-if="record.expanded">
+                        <div class="row q-gutter-x-sm">
+                            <div class="col">
+                                <div class="text-grey">Genotype Quality</div>
+                                <div>{{ record.Genotype_Quality || '-' }}</div>
+                                <div class="text-grey">Variant Quality</div>
+                                <div>{{ record.Variant_Quality || '-' }}</div>
+                                <div class="text-grey">Depth Quality</div>
+                                <div>{{ record.Depth_Quality }}</div>
+                            </div>
+                            <div class="col">
+                                <div class="text-grey">Allele Fraction</div>
+                                <div>{{ formatAF(record) }}%</div>
+                                <div class="text-grey">Depth</div>
+                                <div>{{ record.Seq_Depths_ || '-' }}</div>
+                                <div class="text-grey">Genotype</div>
+                                <div>{{ record.Genotype }}</div>
+                            </div>
+                        </div>
+                    </template>
+                    <template v-else>
+                        <div class="row q-gutter-x-sm">
+                            <div>{{ record.Depth_Quality }}(DP:{{ record.Seq_Depths_ }})</div>
+                            <div>AF: {{ formatAF(record) }}%</div>
+                        </div>
+                    </template>
+                </template>
+                <template v-if="column.dataIndex === 'Gene_Related_Diseases'">
+                    <template v-if="record.expanded">
+                        <template v-for="grd in record.Gene_Related_Diseases" :key="grd">
+                            <a-tooltip :title="grd">
+                                <div>{{ grd }}</div>
+                            </a-tooltip>
+                        </template>
+                    </template>
+                    <template v-else>
+                        <a-tooltip :title="record.Gene_Related_Diseases && record.Gene_Related_Diseases[0]">
+                            <div>{{ record.Gene_Related_Diseases && record.Gene_Related_Diseases[0] }}</div>
+                        </a-tooltip>
+                        <div
+                            v-if="record.Gene_Related_Diseases && record.Gene_Related_Diseases.length > 2"
+                            class="col-1 text-primary cursor-pointer"
+                            @click="record.expanded = !record.expanded"
+                        >
+                            +{{ record.Gene_Related_Diseases.length - 2 }}
+                        </div>
+                    </template>
+                </template>
+                <template v-if="column.dataIndex === 'userVerdict'">
+                    <div>{{ (record.userVerdict || []).join(', ') || '-' }}</div>
+                </template>
+                <template v-if="column.dataIndex === 'ACMG_result'">
+                    <div>{{ record.ACMG_result || '-' }}</div>
+                </template>
+                <template v-if="column.dataIndex === 'Clinvar'">
+                    <div>{{ record.Clinvar || '-' }}</div>
+                </template>
+                <template v-if="column.dataIndex === 'Frequencies'">
+                    <div>My Cohort: {{ record.cohort }}</div>
+                    <div>
+                        <template v-if="record.expanded">
+                            <div v-if='record.gnomAD_genome_ALL !== "."'>
+                                GnomAD Genome Total: {{ record.gnomAD_genome_ALL }}
+                            </div>
+                            <div v-if='record.gnomAD_exome_ALL !== "."'>
+                                GnomAD Exome Total: {{ record.gnomAD_exome_ALL }}
+                            </div>
+                            <div v-if='record.ExAC_ALL !== "."'>ExAC Total: {{ record.ExAC_ALL }}</div>
+                            <div v-if='record["1000g2015aug_all"] !== "."'>
+                                1000g2015aug Total: {{ record['1000g2015aug_all'] }}
+                            </div>
+                        </template>
+                        <template v-else>
+                            <template v-if="getValidFrequencies(record).length <= 2">
+                                <div v-if="getValidFrequencies(record).length === 1">
+                                    {{getValidFrequencies(record)[0].name}}: {{ getValidFrequencies(record)[0].value }}
+                                </div>
+                                <div v-if="getValidFrequencies(record).length === 2">
+                                    {{getValidFrequencies(record)[1].name}}: {{ getValidFrequencies(record)[1].value }}
+                                </div>
+                            </template>
+                            <template v-else>
+                                <div>
+                                    {{getValidFrequencies(record)[0].name}}: {{ getValidFrequencies(record)[0].value }}
+                                </div>
+                                <div>
+                                    {{getValidFrequencies(record)[1].name}}: {{ getValidFrequencies(record)[1].value }}
+                                </div>
+                                <div
+                                    class="text-primary cursor-pointer"
+                                    v-if="getValidFrequencies(record).length > 2"
+                                    @click="record.expanded = !record.expanded"
+                                >
+                                    +{{ getValidFrequencies(record).length - 2 }}
+                                </div>
+                            </template>
+                        </template>
+                    </div>
+                </template>
+                <template v-if="column.dataIndex === 'HPO'">
+                    <template v-if="record.expanded">
+                        <q-scroll-area style="height: 300px">
+                            <template v-for="hpo in record.HPO" :key="hpo">
+                                <a-tooltip :title="hpo">
+                                    <div>{{ hpo }}</div>
+                                </a-tooltip>
+                            </template>
+                        </q-scroll-area>
+                    </template>
+                    <template v-else>
+                        <a-tooltip :title="Array.isArray(record.HPO) ? record.HPO[0] : record.HPO">
+                            <div>{{ Array.isArray(record.HPO) ? record.HPO[0] : record.HPO }}</div>
+                        </a-tooltip>
+                        <div
+                            class="text-primary cursor-pointer"
+                            v-if="Array.isArray(record.HPO) && record.HPO.length > 2"
+                            @click="record.expanded = !record.expanded"
+                        >
+                            +{{ record.HPO.length - 2 }}
+                        </div>
+                    </template>
+                </template>
+                <template v-if="column.dataIndex === 'Software_Prediction_result'">
+                    <div>{{ record.Software_Prediction_result || '-' }}</div>
+                </template>
+                <template v-if="column.dataIndex === 'operation'">
+                    <q-btn
+                        color="negative"
+                        size="sm"
+                        :label="$t('Delete')"
+                        @click="$emit('remove', record.lineNumber)"
+                    />
+                </template>
+            </template>
+        </a-table>
+        <div v-if="!loading && displayRows.length === 0" class="text-grey q-mt-sm">{{$t('NoData')}}</div>
     </div>
-    <a-table
-      :loading="loading"
-      :data-source="displayRows"
-      :columns="columns"
-      :scroll="{ x: 800, y: 450 }"
-      rowKey="lineNumber"
-      :sticky="true"
-      :row-selection="rowSelection"
-    >
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.dataIndex === 'rowNo'">
-          <div>{{ record.lineNumber }}</div>
-        </template>
-        <template v-if="column.dataIndex === 'geneInfo'">
-          <div class="row">
-            <template v-if="record.expanded">
-              <div class="col-2 column justify-between">
-                <div class="col-2 text-weight-bolder text-green-5 q-pl-sm justify-between" style="font-size: 14px">
-                  {{ record.Class }}
-                </div>
-              </div>
-              <div class="col-10 text-left">
-                <div class="text-primary" style="font-size: 16px">{{ record['Gene.refGene'] }}</div>
-                <div>{{ record['ExonicFunc.refGene'] }}</div>
-                <div>{{ record['Chr'] + ':' + record.Start + ' ' + record.Ref + '>' + record.Alt }}</div>
-                <div>{{ record.NUChange }}</div>
-                <div>{{ record.AAChange }}</div>
-                <div>{{ record['GeneDetail.refGene'] + ' ' + record.exon }}</div>
-              </div>
-            </template>
-            <template v-else>
-              <div class="col-2">
-                <div class="text-weight-bolder text-green-5 q-pl-sm" style="font-size: 16px">{{ record.Class }}</div>
-                <div class="text-primary content-center" style="font-size: 16px">{{ record['Gene.refGene'] }}</div>
-              </div>
-              <div class="col-10 text-blue-grey-6 justify-between">
-                <div>{{ record['ExonicFunc.refGene'] }}</div>
-                <div>{{ record.NUChange }}</div>
-              </div>
-            </template>
-          </div>
-        </template>
-        <template v-if="column.dataIndex === 'genoTypeQuality'">
-          <template v-if="record.expanded">
-            <div class="row q-gutter-x-sm">
-              <div class="col">
-                <div class="text-grey">Genotype Quality</div>
-                <div>{{ record.Genotype_Quality || '-' }}</div>
-                <div class="text-grey">Variant Quality</div>
-                <div>{{ record.Variant_Quality || '-' }}</div>
-                <div class="text-grey">Depth Quality</div>
-                <div>{{ record.Depth_Quality }}</div>
-              </div>
-              <div class="col">
-                <div class="text-grey">Allele Fraction</div>
-                <div>{{ formatAF(record) }}%</div>
-                <div class="text-grey">Depth</div>
-                <div>{{ record.Seq_Depths_ || '-' }}</div>
-                <div class="text-grey">Genotype</div>
-                <div>{{ record.Genotype }}</div>
-              </div>
-            </div>
-          </template>
-          <template v-else>
-            <div class="row q-gutter-x-sm">
-              <div>{{ record.Depth_Quality }}(DP:{{ record.Seq_Depths_ }})</div>
-              <div>AF: {{ formatAF(record) }}%</div>
-            </div>
-          </template>
-        </template>
-        <template v-if="column.dataIndex === 'Gene_Related_Diseases'">
-          <template v-if="record.expanded">
-            <template v-for="grd in record.Gene_Related_Diseases" :key="grd">
-              <a-tooltip :title="grd">
-                <div>{{ grd }}</div>
-              </a-tooltip>
-            </template>
-          </template>
-          <template v-else>
-            <a-tooltip :title="record.Gene_Related_Diseases && record.Gene_Related_Diseases[0]">
-              <div>{{ record.Gene_Related_Diseases && record.Gene_Related_Diseases[0] }}</div>
-            </a-tooltip>
-            <div v-if="record.Gene_Related_Diseases && record.Gene_Related_Diseases.length > 2" class="col-1 text-primary cursor-pointer" @click="record.expanded = !record.expanded">
-              +{{ record.Gene_Related_Diseases.length - 2 }}
-            </div>
-          </template>
-        </template>
-        <template v-if="column.dataIndex === 'operation'">
-          <q-btn color="negative" size="sm" :label="$t('Delete')" @click="$emit('remove', record.lineNumber)" />
-        </template>
-      </template>
-    </a-table>
-    <div v-if="!loading && displayRows.length === 0" class="text-grey q-mt-sm">{{$t('NoData')}}</div>
-  </div>
 </template>
 
 <script setup>
 import { ref, watch, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { readTaskMuFile } from 'src/api/task'
+import { listVerdictByPatient } from 'src/api/verdict'
+import { getCohortStatus } from 'src/api/report'
+import { globalStore } from 'src/stores/global'
 
 const props = defineProps({
   selectedKeys: { type: Array, default: () => [] },
@@ -117,15 +228,30 @@ const displayRows = ref([])
 const selectedRowKeys = ref([])
 
 const columns = [
-  { title: 'Row', dataIndex: 'rowNo', width: 80, align: 'center' },
+  { title: '', dataIndex: 'expand', width: 30, align: 'left' },
   { title: 'Gene Info', dataIndex: 'geneInfo', width: 240, align: 'left' },
-  { title: 'Genotype & Quality', dataIndex: 'genoTypeQuality', width: 220, align: 'left' },
+  { title: 'Genotype & Quality', dataIndex: 'genoTypeQuality', width: 170, align: 'left' },
   { title: 'Gene Related Diseases', dataIndex: 'Gene_Related_Diseases', width: 200, align: 'left' },
-  { title: 'Operation', dataIndex: 'operation', width: 120, align: 'center' },
+  { title: 'User Verdict', dataIndex: 'userVerdict', width: 100, align: 'center' },
+  { title: 'ACMG', dataIndex: 'ACMG_result', width: 120, align: 'left' },
+  { title: 'Clinvar', dataIndex: 'Clinvar', width: 80, align: 'left' },
+  { title: 'Frequencies', dataIndex: 'Frequencies', width: 170, align: 'left' },
+  { title: 'Related HPOs', dataIndex: 'HPO', width: 130, align: 'left' },
+  { title: 'Software Prediction', dataIndex: 'Software_Prediction_result', width: 80, align: 'left' },
+  { title: 'Operation', dataIndex: 'operation', width: 80, align: 'center', fixed: 'right' },
 ]
 
 const loadData = async () => {
   loading.value = true
+  const store = globalStore()
+  const verdicts = await listVerdictByPatient(store.currentUser.username)
+  const verdictMap = new Map()
+  verdicts.forEach(v => verdictMap.set(v.gene_identifier, v.result))
+  const cohortData = await getCohortStatus(route.params.id)
+  const cohortMap = new Map()
+  cohortData.forEach(v => {
+    cohortMap.set(v.gene_info, (v.cnt / v.total).toFixed(2))
+  })
   const res = await readTaskMuFile(route.params.id, 'Mut_WES')
   const lines = res.split('\n').filter((t) => t.trim().length > 0)
   const header = lines.shift().split('\t')
@@ -135,10 +261,17 @@ const loadData = async () => {
     const record = { expanded: false }
     header.forEach((h, idx) => (record[h] = values[idx] || ''))
     record.lineNumber = i + 1
+    const gid = `${record['Gene.refGene']}|${record['GeneDetail.refGene']}|${record['AAChange']}|${record.Chr}|${record.Start}|${record.End}|${record.Ref}|${record.Alt}`
+    record.geneIdentifier = gid
+    record.userVerdict = verdictMap.get(gid) || []
+    record.cohort = cohortMap.get(gid) || '-'
     if (record.Gene_Related_Diseases) {
       record.Gene_Related_Diseases = record.Gene_Related_Diseases.split(';').filter((t) => t && t !== '.')
     } else {
       record.Gene_Related_Diseases = []
+    }
+    if (record.HPO) {
+      record.HPO = record.HPO.split(';').filter((t) => t && t !== '.')
     }
     rows.push(record)
   }
@@ -180,6 +313,16 @@ function formatAF(record) {
   n = Number(s)
   if (!isFinite(n)) return '-'
   return (n * 100).toFixed(2)
+}
+
+const getValidFrequencies = (record) => {
+  const frequencies = [
+    { name: 'GnomAD Genome Total', value: record.gnomAD_genome_ALL },
+    { name: 'GnomAD Exome Total', value: record.gnomAD_exome_ALL },
+    { name: 'ExAC Total', value: record.ExAC_ALL },
+    { name: '1000g2015aug Total', value: record['1000g2015aug_all'] },
+  ]
+  return frequencies.filter(freq => freq.value !== '.')
 }
 
 const rowSelection = {
